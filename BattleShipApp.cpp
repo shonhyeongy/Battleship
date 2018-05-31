9136dfc2f8741dd8d7195984615b6448f13b02fd
jjangga <jjangga@kookmin.ac.kr>
2018-05-31 12:41:59 +0900

    add 격추 feature

--- BattleShipApp.cpp
+++ BattleShipApp.cpp
@@ -1,6 +1,6 @@
 #include "BattleShipApp.h"
 #include "BattleShipMap.h"
-
+#include <vector>
 
 void CBattleShipApp::Init() {
 
@@ -39,14 +39,38 @@ void CBattleShipApp::play() {
 
         if (m_pMap->getFromCode(cmd) == '0') {
             m_pMap2->getFromCode(cmd) = 'M';
+            wattron(m_pInputPane->m_pWindow, COLOR_PAIR(4));
+            mvwprintw(m_pInputPane->m_pWindow, 3, 2, "Result : MISS"); // 변경사항
+            wattron(m_pInputPane->m_pWindow, COLOR_PAIR(3));
         } else {
             m_pMap2->getFromCode(cmd) = 'H';
+            wattron(m_pInputPane->m_pWindow, COLOR_PAIR(1));
+            mvwprintw(m_pInputPane->m_pWindow, 3, 2, "Result : SUCCESS"); // 변경사항
+            wattron(m_pInputPane->m_pWindow, COLOR_PAIR(3));
+        }
+
+
+        bool is_destroyed[] = {true, true, true, true}; // A,B,C,D 순서
+        // 따로 배들의 좌표를 저장해놓는 자료구조를 안 만들어 놔서 화면에 H 이 체크된 것을 보고 모두 격추되었는지 is_destroyed 에 기록함
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (m_pMap->m_mapData[i][j] != '0' && m_pMap2->m_mapData[i][j] != 'H') {
+                    is_destroyed[m_pMap->m_mapData[i][j] - 65] = false;
+                }
+            }
         }
         m_pMap2->Draw();
 
-        //m_pStatPane->Draw(1 | 4);
+        int x = 0;
+        int j = 1;
+        for (int k = 0; k < 4; ++k) {
+            x = x | (is_destroyed[k] ? j : 0);
+            j *= 2;
+        }
 
-        mvwprintw(m_pInputPane->m_pWindow, 3, 2, "Result : SUCCESS"); // 변경사항
+        //m_pStatPane->Draw(1 | 4);
+        m_pStatPane->Draw(x);
+
         wrefresh(m_pInputPane->m_pWindow); // 누적된 변경사항 화면에 표시
 
     }
